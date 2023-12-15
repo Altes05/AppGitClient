23daaf6312056e4d1a9d4bb289054d96a7af83d1
Altes05 <151917500+Altes05@users.noreply.github.com>
2023-12-15 10:52:26 +0100

    Add files via upload

---
+++ GitCliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practicas;
+
+/**
+ *
+ * @author altalopa
+ */
+public class GitCliente {
+    
+}

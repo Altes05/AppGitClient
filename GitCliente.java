9e1058809b2aa2232010e14a170b227a9faba0a7
Altes05 <151917500+Altes05@users.noreply.github.com>
2023-12-15 11:28:47 +0100

    Update GitCliente.java

--- GitCliente.java
+++ GitCliente.java
@@ -9,5 +9,36 @@ package Practicas;
  * @author altalopa
  */
 public class GitCliente {
+    private String cliente;
+    private String version;
+    private String sistemaOper;
+    private String licencia;
+    
+    public GitCliente(){
+    }
+    public String getCliente(){
+    return cliente;
+    }
+    public void setCliente(String cliente){
+        this.cliente=cliente;
+    }
+    public String getVerersion(){
+    return version;
+    }
+    public void setVersion(){
+    this.version=version;
+    }
+    public String getSistemOper(String sistemaOper){
+      return sistemaOper;
+    }
+    public void setSistemaOper(String sistemaOper){
+        this.sistemaOper=sistemaOper;
+    }
+    public String getLicencia(){
+        return licencia;
+    }
+    public void setLicencia(String Licencia){
+        this.licencia=licencia;
+    }
     
 }
